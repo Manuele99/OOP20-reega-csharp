b3b6a0e5d344ee77b53bb4b9b28ea7613555c13b
HiiD <salomone.marco@outlook.it>
2021-04-28 19:21:28 +0200

    Created the Navigators

---
+++ Reega/Navigation/INavigator.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reega.Navigation
+{
+    public interface INavigator : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Build a <see cref="IViewModel"/>
+        /// </summary>
+        /// <typeparam name="T">Type of the ViewModel</typeparam>
+        /// <returns> an instance of the ViewModel</returns>
+        T BuildViewModel<T>() where T: IViewModel;
+        /// <summary>
+        /// Push <paramref name="viewModel"/> to the navigation stack
+        /// </summary>
+        /// <param name="viewModel">ViewModel that needs to be pushed</param>
+        /// <param name="clearNavigationStack">Clear the navigation stack before pushing the ViewModel</param>
+        void PushViewModelToStack(IViewModel viewModel, bool clearNavigationStack);
+        /// <summary>
+        /// Pop the current ViewModel that is in the peek of the stack <see cref="SelectedViewModelProperty"/>
+        /// </summary>
+        void PopController();
+        /// <summary>
+        /// Current ViewModel in the scene.
+        /// </summary>
+        IViewModel SelectedViewModel { get; }
+        /// <summary>
+        /// State of the navigation stack
+        /// </summary>
+        bool NavigationStackNotEmpty { get; }
+    }
+}
---
+++ Reega/Navigation/Navigator.cs
@@ -0,0 +1,75 @@
+using Reega.DI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reega.Navigation
+{
+    public class Navigator : INavigator
+    {
+        private IViewModel _selectedViewModel;
+        public IViewModel SelectedViewModel
+        {
+            get => _selectedViewModel;
+            private set
+            {
+                _selectedViewModel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _navigationStackNotEmpty;
+        public bool NavigationStackNotEmpty
+        {
+            get => _navigationStackNotEmpty;
+            set
+            {
+                _navigationStackNotEmpty = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly Stack<IViewModel> _navigationStack = new();
+        private readonly ServiceProvider _serviceProvider;
+
+        public Navigator(ServiceProvider serviceProvider)
+        {
+            this._serviceProvider = serviceProvider;
+        }
+
+        public T BuildViewModel<T>() where T : IViewModel =>
+            this._serviceProvider.GetService<T>() ?? throw new InvalidOperationException();
+
+        public void PopController()
+        {
+            if (NavigationStackNotEmpty)
+            {
+                this._navigationStack.Pop();
+                this.NavigationStackNotEmpty = this._navigationStack.Count > 1;
+                this.SelectedViewModel = this._navigationStack.Peek();
+            }
+        }
+
+        public void PushViewModelToStack(IViewModel viewModel, bool clearNavigationStack)
+        {
+            if (clearNavigationStack)
+                this._navigationStack.Clear();
+
+            this._navigationStack.Push(viewModel);
+            this.NavigationStackNotEmpty = this._navigationStack.Count > 1;
+            this.SelectedViewModel = viewModel;
+
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
