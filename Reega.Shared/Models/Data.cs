a434260909c2de81d21d5034fbe3192bfcc44303
Daniel Benazzi <daniel.benazzi@studio.unibo.it>
2021-04-30 15:18:44 +0200

    formatting

--- Reega.Shared/Models/Data.cs
+++ Reega.Shared/Models/Data.cs
@@ -1,41 +1,37 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Reega.Shared.Models
 {
     class Data
     {
-        private readonly int contractID;
-        private readonly DataType type;
-        private readonly IDictionary<long, double> dataValuesByTimestamp;
+        public int ContractID { get; }
 
-        public Data(int contractID, DataType dataType) : this(contractID,dataType, new Dictionary<long, double> )
+        public DataType Type { get; }
+
+        public IDictionary<long, double> DataValuesByTimestamp { get; }
+
+        public Data(int contractID, DataType dataType) : this(contractID,dataType, new Dictionary<long, double>() )
         {
-            this.contractID = contractID;
-            this.type = dataType;
+            this.ContractID = contractID;
+            this.Type = dataType;
         }
 
         public Data(int contractID, DataType dataType, IDictionary<long, double> data)
         {
-            this.contractID = contractID;
-            this.type = dataType;
-            this.dataValuesByTimestamp = data;
+            this.ContractID = contractID;
+            this.Type = dataType;
+            this.DataValuesByTimestamp = data;
         }
 
-        public void addRecord(long timestamp, double value) 
-        {
-            this.dataValuesByTimestamp.Add(timestamp, value);
-        }
+        public void AddRecord(long timestamp, double value) => 
+            this.DataValuesByTimestamp.Add(timestamp, value);
 
         public void AddRecords(IDictionary<long, double> values)
         {
             foreach (KeyValuePair<long, double> pair in values)
-                this.dataValuesByTimestamp.Add(pair.Key, pair.Value);
+                this.DataValuesByTimestamp.Add(pair.Key, pair.Value);
         }
 
-        public DataType Type => type;
-
-        public int ContractID => contractID;
-
-        public IDictionary<long, double> DataValuesByTimestamp => dataValuesByTimestamp;
     }
 }
