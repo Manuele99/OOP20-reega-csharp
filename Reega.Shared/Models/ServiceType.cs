52c39cdeb5825b35651eebaac3ad0adfa8dba730
Daniel Benazzi <daniel.benazzi@studio.unibo.it>
2021-05-02 17:51:56 +0200

    update ServiceType to mimic Java enum

--- Reega.Shared/Models/ServiceType.cs
+++ Reega.Shared/Models/ServiceType.cs
@@ -1,10 +1,21 @@
 namespace Reega.Shared.Models
 {
-    public enum ServiceType
+    public class ServiceType
     {
-        electricity = 0, 
-        gas = 1,
-        water = 2,
-        garbage = 3
+        public static ServiceType ELECTRICITY => new("electricity", 0);
+        public static ServiceType GAS => new("gas", 1);
+        public static ServiceType WATER => new("water", 2);
+        public static ServiceType GARBAGE => new("garbage", 3);
+
+        public string Name { get; }
+        public int Value { get; }
+
+        private ServiceType() { }
+
+        private ServiceType(string name,int value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
     }
 }
